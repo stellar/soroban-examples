266f6e3e1a6388551af3262f764dd35904aa3d5c
Jonathan Jove <jon@stellar.org>
2022-07-27 11:21:39 -0500

    Modernize testing

--- liquidity_pool/src/external.rs
+++ liquidity_pool/src/external.rs
@@ -1,58 +1,51 @@
 #![cfg(feature = "testutils")]
 
-use num_bigint::BigInt;
-use stellar_contract_sdk::{Binary, Env};
-use stellar_token_contract::external::{Identifier, U256};
-use stellar_xdr::HostFunction;
+use stellar_contract_sdk::{BigInt, Binary, Env, FixedBinary};
+use stellar_token_contract::public_types::Identifier;
 
-pub fn register_test_contract(e: &Env, contract_id: &U256) {
-    let mut bin = Binary::new(e);
-    for b in contract_id {
-        bin.push(*b);
+pub fn register_test_contract(e: &Env, contract_id: &[u8; 32]) {
+    let contract_id = Binary::from_array(e, *contract_id);
+    e.register_contract(contract_id, crate::LiquidityPool {});
+}
+
+pub use crate::__deposit::call_external as deposit;
+pub use crate::__initialize::call_external as initialize;
+pub use crate::__share_id::call_external as share_id;
+pub use crate::__swap::call_external as swap;
+pub use crate::__withdraw::call_external as withdraw;
+
+pub struct LiquidityPool {
+    env: Env,
+    contract_id: Binary,
+}
+
+impl LiquidityPool {
+    pub fn new(env: &Env, contract_id: &[u8; 32]) -> Self {
+        Self {
+            env: env.clone(),
+            contract_id: Binary::from_slice(env, contract_id),
+        }
     }
 
-    e.register_contract(bin.into(), crate::LiquidityPool {});
-}
-
-pub fn initialize(e: &mut Env, contract_id: &U256, token_a: &U256, token_b: &U256) {
-    e.invoke_contract_external(
-        HostFunction::Call,
-        (contract_id, "initialize", token_a, token_b)
-            .try_into()
-            .unwrap(),
-    );
-}
-
-pub fn share_id(e: &mut Env, contract_id: &U256) -> U256 {
-    use stellar_xdr::{ScObject, ScVal};
-    let id = e.invoke_contract_external(
-        HostFunction::Call,
-        (contract_id, "share_id").try_into().unwrap(),
-    );
-    if let ScVal::Object(Some(ScObject::Binary(bin))) = id {
-        bin.as_slice().try_into().unwrap()
-    } else {
-        panic!()
+    pub fn initialize(&mut self, token_a: &[u8; 32], token_b: &[u8; 32]) {
+        let token_a = FixedBinary::from_array(&self.env, *token_a);
+        let token_b = FixedBinary::from_array(&self.env, *token_b);
+        initialize(&mut self.env, &self.contract_id, &token_a, &token_b)
     }
-}
 
-pub fn deposit(e: &mut Env, contract_id: &U256, to: &Identifier) {
-    e.invoke_contract_external(
-        HostFunction::Call,
-        (contract_id, "deposit", to).try_into().unwrap(),
-    );
-}
+    pub fn share_id(&mut self) -> Binary {
+        share_id(&mut self.env, &self.contract_id)
+    }
 
-pub fn swap(e: &mut Env, contract_id: &U256, to: &Identifier, out_a: &BigInt, out_b: &BigInt) {
-    e.invoke_contract_external(
-        HostFunction::Call,
-        (contract_id, "swap", to, out_a, out_b).try_into().unwrap(),
-    );
-}
+    pub fn deposit(&mut self, to: &Identifier) {
+        deposit(&mut self.env, &self.contract_id, to)
+    }
 
-pub fn withdraw(e: &mut Env, contract_id: &U256, to: &Identifier) {
-    e.invoke_contract_external(
-        HostFunction::Call,
-        (contract_id, "withdraw", to).try_into().unwrap(),
-    );
+    pub fn swap(&mut self, to: &Identifier, out_a: &BigInt, out_b: &BigInt) {
+        swap(&mut self.env, &self.contract_id, to, out_a, out_b)
+    }
+
+    pub fn withdraw(&mut self, to: &Identifier) {
+        withdraw(&mut self.env, &self.contract_id, to)
+    }
 }
