d85c99b03d72f3ec63e9b028ecf3f9eb3c33c6af
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-07-29 18:12:14 -0700

    Add cross contract call example

---
+++ cross_contract_calls/src/test.rs
@@ -0,0 +1,31 @@
+#![cfg(test)]
+
+use super::{add_with, ContractA, ContractB};
+use soroban_sdk::{Env, FixedBinary};
+
+#[test]
+fn test() {
+    let env = Env::default();
+
+    let contract_a = FixedBinary::from_array(&env, [0; 32]);
+    env.register_contract(&contract_a, ContractA);
+
+    let contract_b = FixedBinary::from_array(&env, [1; 32]);
+    env.register_contract(&contract_b, ContractB);
+
+    assert_eq!(
+        add_with::invoke(
+            &env,
+            // Invoke 'add_with' on contract B.
+            &contract_b,
+            // Value X.
+            &5,
+            // Value Y.
+            &7,
+            // Tell contract B to call contract A.
+            &contract_a,
+        ),
+        // Expect result 12.
+        12
+    );
+}
