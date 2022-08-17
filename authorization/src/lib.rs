c75c1abf057e33deb8bbbcfdb37907b86f5258f7
Siddharth Suresh <siddharth@stellar.org>
2022-08-17 16:47:37 -0700

    generalize auth

--- authorization/src/lib.rs
+++ authorization/src/lib.rs
@@ -3,10 +3,11 @@
 #[cfg(feature = "testutils")]
 extern crate std;
 
-mod cryptography;
-mod public_types;
+pub mod cryptography;
+pub mod public_types;
 mod test;
 
+use cryptography::NonceAuth;
 use public_types::{Identifier, KeyedAuthorization};
 use soroban_sdk::{contractimpl, contracttype, BigInt, Env, IntoVal, Symbol};
 
@@ -16,6 +17,34 @@ pub enum DataKey {
     Acc(Identifier),
     Nonce(Identifier),
     Admin,
+}
+
+fn read_nonce(e: &Env, id: Identifier) -> BigInt {
+    let key = DataKey::Nonce(id);
+    if let Some(nonce) = e.contract_data().get(key) {
+        nonce.unwrap()
+    } else {
+        BigInt::zero(e)
+    }
+}
+struct WrappedAuth(KeyedAuthorization);
+
+impl NonceAuth for WrappedAuth {
+    fn read_nonce(e: &Env, id: Identifier) -> BigInt {
+        read_nonce(e, id)
+    }
+
+    fn read_and_increment_nonce(&self, e: &Env, id: Identifier) -> BigInt {
+        let key = DataKey::Nonce(id.clone());
+        let nonce = Self::read_nonce(e, id);
+        e.contract_data()
+            .set(key, nonce.clone() + BigInt::from_u32(e, 1));
+        nonce
+    }
+
+    fn get_keyed_auth(&self) -> &KeyedAuthorization {
+        &self.0
+    }
 }
 
 pub struct AuthContract;
@@ -33,15 +62,16 @@ impl AuthContract {
 
     // Saves data that corresponds to an Identifier, with that Identifiers authorization
     pub fn save_data(e: Env, auth: KeyedAuthorization, nonce: BigInt, num: BigInt) {
+        let auth_id = auth.get_identifier(&e);
+
         cryptography::check_auth(
             &e,
-            &auth,
+            &WrappedAuth(auth),
             nonce.clone(),
             Symbol::from_str("save_data"),
             (nonce, num.clone()).into_val(&e),
         );
 
-        let auth_id = auth.get_identifier(&e);
         e.contract_data().set(DataKey::Acc(auth_id), num);
     }
 
@@ -54,7 +84,7 @@ impl AuthContract {
 
         cryptography::check_auth(
             &e,
-            &auth,
+            &WrappedAuth(auth),
             nonce.clone(),
             Symbol::from_str("overwrite"),
             (nonce, id.clone(), num.clone()).into_val(&e),
@@ -64,6 +94,6 @@ impl AuthContract {
     }
 
     pub fn nonce(e: Env, to: Identifier) -> BigInt {
-        cryptography::read_nonce(&e, to)
+        read_nonce(&e, to)
     }
 }
