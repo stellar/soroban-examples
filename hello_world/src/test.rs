5656e34572334d798eaa842b61b3b5b5105468df
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-30 12:44:01 -0700

    Update the hello_world contract (#91)
    * Update the hello_world contract
    
    * change sourbun to dev

--- hello_world/src/test.rs
+++ hello_world/src/test.rs
@@ -1,17 +1,15 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{vec, BytesN, Env};
+use soroban_sdk::{symbol, vec, BytesN, Env};
 
 #[test]
 fn test() {
     let env = Env::default();
     let contract_id = BytesN::from_array(&env, &[0; 32]);
     env.register_contract(&contract_id, HelloContract);
+    let client = HelloContractClient::new(&env, &contract_id);
 
-    let words = HelloContractClient::new(&env, &contract_id).hello(&Symbol::from_str("SourBun"));
-    assert_eq!(
-        words,
-        vec![&env, Symbol::from_str("Hello"), Symbol::from_str("SourBun"),]
-    );
+    let words = client.hello(&symbol!("Dev"));
+    assert_eq!(words, vec![&env, symbol!("Hello"), symbol!("Dev"),]);
 }
