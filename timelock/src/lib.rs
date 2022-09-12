9b37ee83c852e030d11902703ee5cb0ff36f48d1
Dmytro Kozhevin <dmytro@stellar.org>
2022-09-12 16:44:57 -0400

    Claimable balance example contract (demonstrates timelock). (#101)

---
+++ timelock/src/lib.rs
@@ -0,0 +1,196 @@
+//! This contract demonstrates 'timelock' concept and implements a
+//! greatly simplified Claimable Balance (similar to
+//! https://developers.stellar.org/docs/glossary/claimable-balance).
+//! The contract allows to deposit some amount of token and allow another
+//! account(s) claim it before or after provided time point.
+#![no_std]
+#[cfg(feature = "testutils")]
+extern crate std;
+
+use soroban_auth::{
+    check_auth, NonceAuth, {Identifier, Signature},
+};
+use soroban_sdk::{contractimpl, contracttype, BigInt, BytesN, Env, IntoVal, Symbol, Vec};
+
+mod token {
+    soroban_sdk::contractimport!(file = "../soroban_token_contract.wasm");
+}
+
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+    Balance,
+    Nonce(Identifier),
+}
+
+#[derive(Clone)]
+#[contracttype]
+pub enum TimeBoundKind {
+    Before,
+    After,
+}
+
+#[derive(Clone)]
+#[contracttype]
+pub struct TimeBound {
+    pub kind: TimeBoundKind,
+    pub timestamp: u64,
+}
+
+#[derive(Clone)]
+#[contracttype]
+pub struct ClaimableBalance {
+    pub token: BytesN<32>,
+    pub amount: BigInt,
+    pub claimants: Vec<Identifier>,
+    pub time_bound: TimeBound,
+}
+
+pub struct ClaimableBalanceContract;
+
+// The 'timelock' part: check that provided timestamp is before/after
+// the current ledger timestamp.
+fn check_time_bound(env: &Env, time_bound: &TimeBound) -> bool {
+    let ledger_timestamp = env.ledger().timestamp();
+
+    match time_bound.kind {
+        TimeBoundKind::Before => ledger_timestamp <= time_bound.timestamp,
+        TimeBoundKind::After => ledger_timestamp >= time_bound.timestamp,
+    }
+}
+
+// Contract usage pattern (pseudocode):
+// 1. Depositor calls `token.approve(depositor_auth, claimable_balance_contract, 100)`
+//    to allow contract to withdraw the needed amount of token.
+// 2. Depositor calls `deposit(depositor_auth, token_id, 100, claimants, time bound)`. Contract
+//    withdraws the provided token amount and stores it until one of the claimants
+//    claims it.
+// 3. Claimant calls `claim(claimant_auth)` and if time/auth conditons are passed
+//    receives the balance.
+#[contractimpl]
+impl ClaimableBalanceContract {
+    pub fn deposit(
+        env: Env,
+        from: Signature,
+        token: BytesN<32>,
+        amount: BigInt,
+        claimants: Vec<Identifier>,
+        time_bound: TimeBound,
+    ) {
+        if claimants.len() > 10 {
+            panic!("too many claimants");
+        }
+
+        let from_id = from.get_identifier(&env);
+        // Authenticate depositor with nonce of zero, so that this may
+        // be successfully called just once.
+        check_auth(
+            &env,
+            &NonceForSignature(from),
+            BigInt::zero(&env),
+            Symbol::from_str("deposit"),
+            (&from_id, &token, &amount, &claimants, &time_bound).into_val(&env),
+        );
+        // Transfer token to this contract address.
+        transfer_from(&env, &token, &from_id, &get_contract_id(&env), &amount);
+        // Store all the necessary balance to allow one of the claimants to claim it.
+        env.contract_data().set(
+            DataKey::Balance,
+            ClaimableBalance {
+                token,
+                amount,
+                time_bound,
+                claimants,
+            },
+        );
+    }
+
+    pub fn claim(env: Env, claimant: Signature) {
+        if !is_initialized(&env) {
+            panic!("contract hasn't been initialized");
+        }
+        let claimable_balance: ClaimableBalance =
+            env.contract_data().get_unchecked(DataKey::Balance).unwrap();
+
+        if !check_time_bound(&env, &claimable_balance.time_bound) {
+            panic!("time predicate is not fulfilled");
+        }
+
+        let claimant_id = claimant.get_identifier(&env);
+        let claimants = &claimable_balance.claimants;
+        if !claimants.contains(&claimant_id) {
+            panic!("claimant is not allowed to claim this balance");
+        }
+        // Authenticate claimant with nonce of zero, so that this may be
+        // successfully called just once.
+        // For simplicity, depositor can't be the claimant.
+        check_auth(
+            &env,
+            &NonceForSignature(claimant),
+            BigInt::zero(&env),
+            Symbol::from_str("claim"),
+            (&claimant_id,).into_val(&env),
+        );
+        // Transfer the stored amount of token to claimant after passing
+        // all the checks.
+        transfer_to(
+            &env,
+            &claimable_balance.token,
+            &claimant_id,
+            &claimable_balance.amount,
+        );
+        // Cleanup unnecessary balance entry.
+        env.contract_data().remove(DataKey::Balance);
+    }
+}
+
+fn is_initialized(env: &Env) -> bool {
+    env.contract_data().has(DataKey::Balance)
+}
+
+fn get_contract_id(e: &Env) -> Identifier {
+    Identifier::Contract(e.get_current_contract().into())
+}
+
+fn transfer_from(
+    e: &Env,
+    token_id: &BytesN<32>,
+    from: &Identifier,
+    to: &Identifier,
+    amount: &BigInt,
+) {
+    let client = token::ContractClient::new(&e, token_id);
+    client.xfer_from(&Signature::Contract, &BigInt::zero(&e), &from, &to, &amount);
+}
+
+fn transfer_to(e: &Env, token_id: &BytesN<32>, to: &Identifier, amount: &BigInt) {
+    let client = token::ContractClient::new(&e, token_id);
+    client.xfer(&Signature::Contract, &BigInt::zero(&e), to, amount);
+}
+
+struct NonceForSignature(Signature);
+
+impl NonceAuth for NonceForSignature {
+    fn read_nonce(e: &Env, id: Identifier) -> BigInt {
+        let key = DataKey::Nonce(id);
+        if let Some(nonce) = e.contract_data().get(key) {
+            nonce.unwrap()
+        } else {
+            BigInt::zero(e)
+        }
+    }
+
+    fn read_and_increment_nonce(&self, e: &Env, id: Identifier) -> BigInt {
+        let key = DataKey::Nonce(id.clone());
+        let nonce = Self::read_nonce(e, id);
+        e.contract_data()
+            .set(key, nonce.clone() + BigInt::from_u32(e, 1));
+        nonce
+    }
+
+    fn signature(&self) -> &Signature {
+        &self.0
+    }
+}
+
+mod test;
---
+++ timelock/src/test.rs
@@ -0,0 +1,335 @@
+#![cfg(test)]
+
+use super::*;
+use ed25519_dalek::Keypair;
+use rand::{thread_rng, RngCore};
+use soroban_auth::{Ed25519Signature, Identifier, SignaturePayload, SignaturePayloadV0};
+use soroban_sdk::testutils::ed25519::Sign;
+use soroban_sdk::testutils::LedgerInfo;
+use soroban_sdk::{vec, Env, RawVal};
+use soroban_token_contract::testutils::{
+    register_test_contract as register_token, to_ed25519, Token,
+};
+
+fn generate_keypair() -> Keypair {
+    Keypair::generate(&mut thread_rng())
+}
+
+fn generate_contract_id() -> [u8; 32] {
+    let mut id: [u8; 32] = Default::default();
+    thread_rng().fill_bytes(&mut id);
+    id
+}
+
+fn create_token_contract(e: &Env, admin: &Keypair) -> (BytesN<32>, Token) {
+    let id = generate_contract_id();
+    register_token(&e, &id);
+    let token = Token::new(e, &id);
+    // decimals, name, symbol don't matter in tests
+    token.initialize(&to_ed25519(&e, admin), 7, "name", "symbol");
+
+    (BytesN::from_array(&e, &id), token)
+}
+
+fn create_claimable_balance_contract(e: &Env) -> ClaimableBalanceContractClient {
+    let contract_id = BytesN::from_array(e, &generate_contract_id());
+    e.register_contract(&contract_id, ClaimableBalanceContract {});
+
+    ClaimableBalanceContractClient::new(e, contract_id)
+}
+
+fn sign_args(
+    env: &Env,
+    signer: &Keypair,
+    fn_name: &str,
+    contract_id: &BytesN<32>,
+    args: Vec<RawVal>,
+) -> Signature {
+    let msg = SignaturePayload::V0(SignaturePayloadV0 {
+        function: Symbol::from_str(fn_name),
+        contract: contract_id.clone(),
+        network: env.ledger().network_passphrase(),
+        args,
+    });
+    sign_payload(env, signer, msg)
+}
+
+fn sign_payload(env: &Env, signer: &Keypair, payload: SignaturePayload) -> Signature {
+    Signature::Ed25519(Ed25519Signature {
+        public_key: signer.public.to_bytes().into_val(env),
+        signature: signer.sign(payload).unwrap().into_val(env),
+    })
+}
+
+struct ClaimableBalanceTest {
+    env: Env,
+    deposit_user: Keypair,
+    claim_users: [Keypair; 3],
+    token: Token,
+    token_id: BytesN<32>,
+    contract: ClaimableBalanceContractClient,
+    contract_id: Identifier,
+}
+
+impl ClaimableBalanceTest {
+    fn setup() -> Self {
+        let env: Env = Default::default();
+        env.set_ledger(LedgerInfo {
+            timestamp: 12345,
+            protocol_version: 1,
+            sequence_number: 10,
+            network_id: Default::default(),
+        });
+
+        let deposit_user = generate_keypair();
+
+        let claim_users = [generate_keypair(), generate_keypair(), generate_keypair()];
+
+        let token_admin = generate_keypair();
+        let (token_id, token) = create_token_contract(&env, &token_admin);
+        token.mint(
+            &token_admin,
+            &to_ed25519(&env, &deposit_user),
+            &BigInt::from_u32(&env, 1000),
+        );
+
+        let contract = create_claimable_balance_contract(&env);
+        let contract_id = Identifier::Contract(contract.contract_id.clone());
+        ClaimableBalanceTest {
+            env,
+            deposit_user,
+            claim_users,
+            token,
+            token_id,
+            contract,
+            contract_id: contract_id,
+        }
+    }
+
+    fn approve_deposit(&self, amount: u32) {
+        self.token.approve(
+            &self.deposit_user,
+            &Identifier::Contract(self.contract.contract_id.clone()),
+            &BigInt::from_u32(&self.env, amount),
+        );
+    }
+
+    fn deposit(&self, amount: u32, claimants: &Vec<Identifier>, time_bound: TimeBound) {
+        self.call_deposit(
+            &self.deposit_user,
+            &self.token_id,
+            &BigInt::from_u32(&self.env, amount),
+            claimants,
+            &time_bound,
+        );
+    }
+
+    fn claim(&self, claim_user: &Keypair) {
+        self.call_claim(claim_user);
+    }
+
+    fn signer_to_id(&self, signer: &Keypair) -> Identifier {
+        to_ed25519(&self.env, &signer)
+    }
+
+    fn call_deposit(
+        &self,
+        signer: &Keypair,
+        token: &BytesN<32>,
+        amount: &BigInt,
+        claimants: &Vec<Identifier>,
+        time_bound: &TimeBound,
+    ) {
+        let signer_id = self.signer_to_id(signer);
+        let args: Vec<RawVal> =
+            (&signer_id, token, amount, claimants, time_bound).into_val(&self.env);
+        let signature = sign_args(
+            &self.env,
+            signer,
+            "deposit",
+            &self.contract.contract_id,
+            args,
+        );
+        self.contract
+            .deposit(&signature, token, amount, claimants, time_bound);
+    }
+
+    fn call_claim(&self, signer: &Keypair) {
+        let signer_id = self.signer_to_id(signer);
+        let args: Vec<RawVal> = (&signer_id,).into_val(&self.env);
+        let signature = sign_args(&self.env, signer, "claim", &self.contract.contract_id, args);
+        self.contract.claim(&signature);
+    }
+}
+
+#[test]
+fn test_deposit_and_claim() {
+    let test = ClaimableBalanceTest::setup();
+    test.approve_deposit(800);
+    test.deposit(
+        800,
+        &vec![
+            &test.env,
+            test.signer_to_id(&test.claim_users[0]),
+            test.signer_to_id(&test.claim_users[1]),
+        ],
+        TimeBound {
+            kind: TimeBoundKind::Before,
+            timestamp: 12346,
+        },
+    );
+
+    assert_eq!(
+        test.token.balance(&test.signer_to_id(&test.deposit_user)),
+        BigInt::from_u32(&test.env, 200)
+    );
+    assert_eq!(
+        test.token.balance(&test.contract_id),
+        BigInt::from_u32(&test.env, 800)
+    );
+    assert_eq!(
+        test.token.balance(&test.signer_to_id(&test.claim_users[1])),
+        BigInt::from_u32(&test.env, 0)
+    );
+
+    test.claim(&test.claim_users[1]);
+    assert_eq!(
+        test.token.balance(&test.signer_to_id(&test.deposit_user)),
+        BigInt::from_u32(&test.env, 200)
+    );
+    assert_eq!(
+        test.token.balance(&test.contract_id),
+        BigInt::from_u32(&test.env, 0)
+    );
+    assert_eq!(
+        test.token.balance(&test.signer_to_id(&test.claim_users[1])),
+        BigInt::from_u32(&test.env, 800)
+    );
+}
+
+#[test]
+#[should_panic(expected = "insufficient allowance")]
+fn test_deposit_above_allowance_not_possible() {
+    let test = ClaimableBalanceTest::setup();
+    test.approve_deposit(800);
+    test.deposit(
+        801,
+        &vec![&test.env, test.signer_to_id(&test.claim_users[0])],
+        TimeBound {
+            kind: TimeBoundKind::Before,
+            timestamp: 12346,
+        },
+    );
+}
+
+#[test]
+#[should_panic(expected = "incorrect nonce")]
+fn test_double_deposit_not_possible() {
+    let test = ClaimableBalanceTest::setup();
+    test.approve_deposit(800);
+    test.deposit(
+        1,
+        &vec![&test.env, test.signer_to_id(&test.claim_users[0])],
+        TimeBound {
+            kind: TimeBoundKind::Before,
+            timestamp: 12346,
+        },
+    );
+    test.deposit(
+        1,
+        &vec![&test.env, test.signer_to_id(&test.claim_users[0])],
+        TimeBound {
+            kind: TimeBoundKind::Before,
+            timestamp: 12346,
+        },
+    );
+}
+
+#[test]
+#[should_panic(expected = "claimant is not allowed to claim this balance")]
+fn test_unauthorized_claim_not_possible() {
+    let test = ClaimableBalanceTest::setup();
+    test.approve_deposit(800);
+    test.deposit(
+        800,
+        &vec![
+            &test.env,
+            test.signer_to_id(&test.claim_users[0]),
+            test.signer_to_id(&test.claim_users[1]),
+        ],
+        TimeBound {
+            kind: TimeBoundKind::Before,
+            timestamp: 12346,
+        },
+    );
+
+    test.claim(&test.claim_users[2]);
+}
+
+#[test]
+#[should_panic(expected = "time predicate is not fulfilled")]
+fn test_out_of_time_bound_claim_not_possible() {
+    let test = ClaimableBalanceTest::setup();
+    test.approve_deposit(800);
+    test.deposit(
+        800,
+        &vec![&test.env, test.signer_to_id(&test.claim_users[0])],
+        TimeBound {
+            kind: TimeBoundKind::After,
+            timestamp: 12346,
+        },
+    );
+
+    test.claim(&test.claim_users[0]);
+}
+
+#[test]
+#[should_panic(expected = "contract hasn't been initialized")]
+fn test_double_claim_not_possible() {
+    let test = ClaimableBalanceTest::setup();
+    test.approve_deposit(800);
+    test.deposit(
+        800,
+        &vec![&test.env, test.signer_to_id(&test.claim_users[0])],
+        TimeBound {
+            kind: TimeBoundKind::Before,
+            timestamp: 12346,
+        },
+    );
+
+    test.claim(&test.claim_users[0]);
+    assert_eq!(
+        test.token.balance(&test.signer_to_id(&test.claim_users[0])),
+        BigInt::from_u32(&test.env, 800)
+    );
+    test.claim(&test.claim_users[0]);
+}
+
+#[test]
+#[should_panic(expected = "incorrect nonce")]
+fn test_deposit_after_claim_not_possible() {
+    let test = ClaimableBalanceTest::setup();
+    test.approve_deposit(1000);
+    test.deposit(
+        800,
+        &vec![&test.env, test.signer_to_id(&test.claim_users[0])],
+        TimeBound {
+            kind: TimeBoundKind::After,
+            timestamp: 12344,
+        },
+    );
+
+    test.claim(&test.claim_users[0]);
+    assert_eq!(
+        test.token.balance(&test.signer_to_id(&test.claim_users[0])),
+        BigInt::from_u32(&test.env, 800)
+    );
+    test.deposit(
+        200,
+        &vec![&test.env, test.signer_to_id(&test.claim_users[0])],
+        TimeBound {
+            kind: TimeBoundKind::After,
+            timestamp: 12344,
+        },
+    );
+}
