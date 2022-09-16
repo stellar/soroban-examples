27ad86dd6d0546a0fc05e8777fe7c6dc3295aeb8
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-15 20:59:09 -0700

    remove export

--- single_offer/src/lib.rs
+++ single_offer/src/lib.rs
@@ -181,8 +181,7 @@ pub trait SingleOfferTrait {
 
 pub struct SingleOffer;
 
-#[cfg_attr(feature = "export", contractimpl)]
-#[cfg_attr(not(feature = "export"), contractimpl(export = false))]
+#[contractimpl]
 impl SingleOfferTrait for SingleOffer {
     fn initialize(
         e: Env,
