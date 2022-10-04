e0088a1e0e05b607db7ea4ad7e0f8ffbbf53a1f6
Siddharth Suresh <siddharth@stellar.org>
2022-10-03 18:01:19 -0700

    update single offer router

--- single_offer_router/src/token_contract.rs
+++ single_offer_router/src/token_contract.rs
@@ -1,4 +1,2 @@
-soroban_sdk::contractimport!(
-    file = "../target/wasm32-unknown-unknown/release/soroban_token_contract.wasm"
-);
+soroban_sdk::contractimport!(file = "../soroban_token_spec.wasm");
 pub type TokenClient = Client;
