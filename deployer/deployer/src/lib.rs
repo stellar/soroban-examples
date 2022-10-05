c630071c9adb321ff15a953ecddd7120c249115d
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-10-05 15:25:26 -0700

    Add comments to deployer example

--- deployer/deployer/src/lib.rs
+++ deployer/deployer/src/lib.rs
@@ -6,6 +6,9 @@ pub struct Deployer;
 
 #[contractimpl]
 impl Deployer {
+    /// Deploy the contract wasm and after deployment invoke the init function
+    /// of the contract with the given arguments. Returns the contract ID and
+    /// result of the init function.
     pub fn deploy(
         env: Env,
         salt: Bytes,
@@ -13,8 +16,12 @@ impl Deployer {
         init_fn: Symbol,
         init_args: Vec<RawVal>,
     ) -> (BytesN<32>, RawVal) {
+        // Deploy the wasm.
         let id = env.deployer().with_current_contract(salt).deploy(wasm);
+        // Invoke the init function with the given arguments.
         let res: RawVal = env.invoke_contract(&id, &init_fn, init_args);
+        // Return the contract ID of the deployed contract and the result of
+        // invoking the init result.
         (id, res)
     }
 }
