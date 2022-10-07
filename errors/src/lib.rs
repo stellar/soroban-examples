6cc2f11bb4fc579fb461400c8f2ae4f682d30e58
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-10-07 16:50:01 -0700

    Add errors example (#138)

---
+++ errors/src/lib.rs
@@ -0,0 +1,46 @@
+#![no_std]
+use soroban_sdk::{contracterror, contractimpl, log, symbol, Env, Symbol};
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    LimitReached = 1,
+}
+
+const COUNTER: Symbol = symbol!("COUNTER");
+const MAX: u32 = 10;
+
+pub struct IncrementContract;
+
+#[contractimpl]
+impl IncrementContract {
+    /// Increment increments an internal counter, and returns the value. Errors
+    /// if the value is attempted to be incremented past 10.
+    pub fn increment(env: Env) -> Result<u32, Error> {
+        // Get the current count.
+        let mut count: u32 = env
+            .data()
+            .get(COUNTER)
+            .unwrap_or(Ok(0)) // If no value set, assume 0.
+            .unwrap(); // Panic if the value of COUNTER is not u32.
+        log!(&env, "count: {}", count);
+
+        // Increment the count.
+        count += 1;
+
+        // Check if the count exceeds the max.
+        if count <= MAX {
+            // Save the count.
+            env.data().set(COUNTER, count);
+
+            // Return the count to the caller.
+            Ok(count)
+        } else {
+            // Return an error if the max is exceeded.
+            Err(Error::LimitReached)
+        }
+    }
+}
+
+mod test;
