2b29013795ea53356a60fa4e564aab53f4a434bf
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-10-07 23:06:16 -0700

    Change the events contract to be an adaption of the increment example (#141)

--- events/src/test.rs
+++ events/src/test.rs
@@ -1,13 +1,37 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{symbol, Env};
+use soroban_sdk::{testutils::Events, vec, Env, IntoVal};
 
 #[test]
 fn test() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, EventsContract);
-    let client = EventsContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, IncrementContract);
+    let client = IncrementContractClient::new(&env, &contract_id);
 
-    client.hello(&symbol!("Dev"));
+    assert_eq!(client.increment(), 1);
+    assert_eq!(client.increment(), 2);
+    assert_eq!(client.increment(), 3);
+
+    assert_eq!(
+        env.events().all(),
+        vec![
+            &env,
+            (
+                contract_id.clone(),
+                (symbol!("COUNTER"), symbol!("increment")).into_val(&env),
+                1u32.into_val(&env)
+            ),
+            (
+                contract_id.clone(),
+                (symbol!("COUNTER"), symbol!("increment")).into_val(&env),
+                2u32.into_val(&env)
+            ),
+            (
+                contract_id.clone(),
+                (symbol!("COUNTER"), symbol!("increment")).into_val(&env),
+                3u32.into_val(&env)
+            ),
+        ]
+    );
 }
