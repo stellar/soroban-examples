54126d417fc271520a832f6136d42997453efa54
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-10-07 20:37:23 -0700

    Revert "Change the events contract to be an adaption of the increment example"
    This reverts commit 3a58cf1bbbe5245900010d99d5f4facdcd2de3f7.

--- events/src/test.rs
+++ events/src/test.rs
@@ -1,32 +1,13 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{
-    symbol,
-    testutils::{Events, Logger},
-    Env,
-};
-
-extern crate std;
+use soroban_sdk::{symbol, Env};
 
 #[test]
 fn test() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, IncrementContract);
-    let client = IncrementContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, EventsContract);
+    let client = EventsContractClient::new(&env, &contract_id);
 
-    assert_eq!(client.increment(), 1);
-    assert_eq!(client.increment(), 2);
-    assert_eq!(client.increment(), 3);
-
-    std::println!("{}", env.logger().all().join("\n"));
-
-    env.events().all().iter().map(Result::unwrap).for_each(|e| {
-        std::println!(
-            "event:\n - contract: {:?}\n - topics: {:?}\n - value: {:?}",
-            e.0,
-            e.1,
-            e.2
-        );
-    });
+    client.hello(&symbol!("Dev"));
 }
