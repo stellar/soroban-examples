d86ba68985009a488eaffc830aeeb9429abc95ac
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-10-07 23:43:51 -0700

    Rewrite the custom types example to use increment example

--- custom_types/src/test.rs
+++ custom_types/src/test.rs
@@ -1,26 +1,25 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::Env;
+use soroban_sdk::{testutils::Logger, Env};
+
+extern crate std;
 
 #[test]
 fn test() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, CustomTypesContract);
-    let client = CustomTypesContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, IncrementContract);
+    let client = IncrementContractClient::new(&env, &contract_id);
 
-    assert_eq!(client.retrieve(), Name::None);
-
-    client.store(&Name::FirstLast(FirstLast {
-        first: symbol!("first"),
-        last: symbol!("last"),
-    }));
-
+    assert_eq!(client.increment(&1), 1);
+    assert_eq!(client.increment(&10), 11);
     assert_eq!(
-        client.retrieve(),
-        Name::FirstLast(FirstLast {
-            first: symbol!("first"),
-            last: symbol!("last"),
-        }),
+        client.get_state(),
+        State {
+            count: 11,
+            last_incr: 10
+        }
     );
+
+    std::println!("{}", env.logger().all().join("\n"));
 }
