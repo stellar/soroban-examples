73e258a2ba6f713a0895aa95f4d0f9cdcdc473bb
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-10-08 21:06:10 -0700

    Change advanced-auth example to be built on increment example (#144)

--- auth_advanced/src/lib.rs
+++ auth_advanced/src/lib.rs
@@ -1,90 +1,91 @@
 #![no_std]
 
-#[cfg(feature = "testutils")]
-extern crate std;
-
-mod test;
-
 use soroban_auth::{
     verify, {Identifier, Signature},
 };
-use soroban_sdk::{contractimpl, contracttype, symbol, BigInt, Env};
+use soroban_sdk::{contracterror, contractimpl, contracttype, panic_error, symbol, BigInt, Env};
 
 #[contracttype]
 pub enum DataKey {
-    SavedNum(Identifier),
+    Counter(Identifier),
     Nonce(Identifier),
-    Admin,
 }
 
-fn read_nonce(e: &Env, id: &Identifier) -> BigInt {
+#[contracterror]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Error {
+    IncorrectNonceForInvoker = 1,
+    IncorrectNonce = 2,
+}
+
+pub struct IncrementContract;
+
+#[contractimpl]
+impl IncrementContract {
+    /// Increment increments a counter for the invoker, and returns the value.
+    pub fn increment(env: Env, sig: Signature, nonce: BigInt) -> u32 {
+        // Verify that the signature signs and authorizes this invocation.
+        let id = sig.identifier(&env);
+        verify(&env, &sig, symbol!("increment"), (&id, &nonce));
+
+        // Verify that the nonce has not been consumed to prevent replay of the
+        // same presigned invocation more than once.
+        verify_and_consume_nonce(&env, &sig, &nonce);
+
+        // Construct a key for the data being stored. Use an enum to set the
+        // contract up well for adding other types of data to be stored.
+        let key = DataKey::Counter(id);
+
+        // Get the current count for the invoker.
+        let mut count: u32 = env
+            .data()
+            .get(&key)
+            .unwrap_or(Ok(0)) // If no value set, assume 0.
+            .unwrap(); // Panic if the value of COUNTER is not u32.
+
+        // Increment the count.
+        count += 1;
+
+        // Save the count.
+        env.data().set(&key, count);
+
+        // Return the count to the caller.
+        count
+    }
+
+    pub fn nonce(env: Env, id: Identifier) -> BigInt {
+        get_nonce(&env, &id)
+    }
+}
+
+fn verify_and_consume_nonce(env: &Env, auth: &Signature, nonce: &BigInt) {
+    match auth {
+        Signature::Invoker => {
+            if BigInt::zero(env) != nonce {
+                panic_error!(env, Error::IncorrectNonceForInvoker);
+            }
+        }
+        Signature::Ed25519(_) | Signature::Account(_) => {
+            let id = auth.identifier(env);
+            if nonce != &get_nonce(env, &id) {
+                panic_error!(env, Error::IncorrectNonce);
+            }
+            set_nonce(env, &id, nonce + 1);
+        }
+    }
+}
+
+fn get_nonce(env: &Env, id: &Identifier) -> BigInt {
     let key = DataKey::Nonce(id.clone());
-    e.data()
+    env.data()
         .get(key)
-        .unwrap_or_else(|| Ok(BigInt::zero(e)))
+        .unwrap_or_else(|| Ok(BigInt::zero(env)))
         .unwrap()
 }
 
-fn verify_and_consume_nonce(e: &Env, auth: &Signature, expected_nonce: &BigInt) {
-    match auth {
-        Signature::Invoker => {
-            if BigInt::zero(&e) != expected_nonce {
-                panic!("nonce should be zero for Invoker")
-            }
-            return;
-        }
-        _ => {}
-    }
-
-    let id = auth.identifier(&e);
+fn set_nonce(env: &Env, id: &Identifier, nonce: BigInt) {
     let key = DataKey::Nonce(id.clone());
-    let nonce = read_nonce(e, &id);
-
-    if nonce != expected_nonce {
-        panic!("incorrect nonce")
-    }
-    e.data().set(key, &nonce + 1);
+    env.data().set(key, nonce);
 }
 
-pub struct ExampleContract;
-
-#[contractimpl]
-impl ExampleContract {
-    /// Set the admin identifier. May be called only once.
-    pub fn set_admin(e: Env, admin: Identifier) {
-        if e.data().has(DataKey::Admin) {
-            panic!("admin is already set")
-        }
-
-        e.data().set(DataKey::Admin, admin);
-    }
-
-    /// Save the number for an authenticated [Identifier].
-    pub fn save_num(e: Env, sig: Signature, nonce: BigInt, num: BigInt) {
-        verify_and_consume_nonce(&e, &sig, &nonce);
-
-        let auth_id = sig.identifier(&e);
-
-        verify(&e, &sig, symbol!("save_num"), (&auth_id, nonce, &num));
-
-        e.data().set(DataKey::SavedNum(auth_id), num);
-    }
-
-    // The admin can write data for any Identifier
-    pub fn overwrite(e: Env, sig: Signature, nonce: BigInt, id: Identifier, num: BigInt) {
-        let auth_id = sig.identifier(&e);
-        if auth_id != e.data().get_unchecked(DataKey::Admin).unwrap() {
-            panic!("not authorized by admin")
-        }
-
-        verify_and_consume_nonce(&e, &sig, &nonce);
-
-        verify(&e, &sig, symbol!("overwrite"), (auth_id, nonce, &id, &num));
-
-        e.data().set(DataKey::SavedNum(id), num);
-    }
-
-    pub fn nonce(e: Env, id: Identifier) -> BigInt {
-        read_nonce(&e, &id)
-    }
-}
+mod test;
