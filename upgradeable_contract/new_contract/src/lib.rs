2e67318c26e2424f0014656f553a641ceb1c5af0
Siddharth Suresh <siddharth@stellar.org>
2023-05-02 17:46:26 -0500

    Bump SDK and add upgradeable_contract example (#238)
    * Bump SDK and add upgradeable_contract example
    
    * Add a function to the new contract
    
    * build new_contract in Makefile

---
+++ upgradeable_contract/new_contract/src/lib.rs
@@ -0,0 +1,33 @@
+#![no_std]
+
+use soroban_sdk::{contractimpl, contracttype, Address, BytesN, Env};
+
+#[contracttype]
+#[derive(Clone)]
+enum DataKey {
+    Admin,
+}
+
+pub struct UpgradeableContract;
+
+#[contractimpl]
+impl UpgradeableContract {
+    pub fn init(e: Env, admin: Address) {
+        e.storage().set(&DataKey::Admin, &admin);
+    }
+
+    pub fn version() -> u32 {
+        2
+    }
+
+    pub fn new_v2_fn() -> u32 {
+        1010101
+    }
+
+    pub fn upgrade(e: Env, new_wasm_hash: BytesN<32>) {
+        let admin: Address = e.storage().get_unchecked(&DataKey::Admin).unwrap();
+        admin.require_auth();
+
+        e.update_current_contract_wasm(&new_wasm_hash);
+    }
+}
---
+++ upgradeable_contract/old_contract/src/test.rs
@@ -0,0 +1,45 @@
+#![cfg(test)]
+
+use soroban_sdk::{testutils::Address as _, Address, BytesN, Env};
+
+mod old_contract {
+    soroban_sdk::contractimport!(
+        file =
+            "target/wasm32-unknown-unknown/release/soroban_upgradeable_contract_old_contract.wasm"
+    );
+}
+
+mod new_contract {
+    soroban_sdk::contractimport!(
+        file = "../new_contract/target/wasm32-unknown-unknown/release/soroban_upgradeable_contract_new_contract.wasm"
+    );
+}
+
+fn install_new_wasm(e: &Env) -> BytesN<32> {
+    e.install_contract_wasm(new_contract::WASM)
+}
+
+#[test]
+fn test() {
+    let env = Env::default();
+
+    // Note that we use register_contract_wasm instead of register_contract
+    // because the old contracts WASM is expected to exist in storage.
+    let contract_id = env.register_contract_wasm(None, old_contract::WASM);
+
+    let client = old_contract::Client::new(&env, &contract_id);
+    let admin = Address::random(&env);
+    client.init(&admin);
+
+    assert_eq!(1, client.version());
+
+    let new_wasm_hash = install_new_wasm(&env);
+
+    client.upgrade(&new_wasm_hash);
+    assert_eq!(2, client.version());
+
+    // new_v2_fn was added in the new contract, so the existing
+    // client is out of date. Generate a new one.
+    let client = new_contract::Client::new(&env, &contract_id);
+    assert_eq!(1010101, client.new_v2_fn());
+}
