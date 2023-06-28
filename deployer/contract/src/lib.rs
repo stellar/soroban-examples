01df93d5c8533611e32bc3a6a86d10df1699bcb0
Dmytro Kozhevin <dmytro@stellar.org>
2023-06-28 13:16:28 -0400

    Bump SDK to 0.9.1

--- deployer/contract/src/lib.rs
+++ deployer/contract/src/lib.rs
@@ -10,7 +10,7 @@ const KEY: Symbol = symbol_short!("value");
 #[contractimpl]
 impl Contract {
     pub fn init(env: Env, value: u32) {
-        env.storage().instance().set(&KEY, &value, None);
+        env.storage().instance().set(&KEY, &value);
     }
     pub fn value(env: Env) -> u32 {
         env.storage().instance().get(&KEY).unwrap()
