a27fe2f72f4f42aec327d1d54690228445726bc8
Siddharth Suresh <siddharth@stellar.org>
2023-06-28 09:12:44 -0700

    Bump sdk for token and implement temporary allowances

--- token/src/balance.rs
+++ token/src/balance.rs
@@ -3,8 +3,8 @@ use soroban_sdk::{Address, Env};
 
 pub fn read_balance(e: &Env, addr: Address) -> i128 {
     let key = DataKey::Balance(addr);
-    if let Some(balance) = e.storage().get(&key) {
-        balance.unwrap()
+    if let Some(balance) = e.storage().persistent().get::<DataKey, i128>(&key) {
+        balance
     } else {
         0
     }
@@ -12,7 +12,7 @@ pub fn read_balance(e: &Env, addr: Address) -> i128 {
 
 fn write_balance(e: &Env, addr: Address, amount: i128) {
     let key = DataKey::Balance(addr);
-    e.storage().set(&key, &amount);
+    e.storage().persistent().set(&key, &amount);
 }
 
 pub fn receive_balance(e: &Env, addr: Address, amount: i128) {
@@ -36,8 +36,8 @@ pub fn spend_balance(e: &Env, addr: Address, amount: i128) {
 
 pub fn is_authorized(e: &Env, addr: Address) -> bool {
     let key = DataKey::State(addr);
-    if let Some(state) = e.storage().get(&key) {
-        state.unwrap()
+    if let Some(state) = e.storage().persistent().get::<DataKey, bool>(&key) {
+        state
     } else {
         true
     }
@@ -45,5 +45,5 @@ pub fn is_authorized(e: &Env, addr: Address) -> bool {
 
 pub fn write_authorization(e: &Env, addr: Address, is_authorized: bool) {
     let key = DataKey::State(addr);
-    e.storage().set(&key, &is_authorized);
+    e.storage().persistent().set(&key, &is_authorized);
 }
