18641cfb3d9760c48b6a00c6705a17e8c035b6f3
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-09-01 16:43:37 -0700

    Simplify token example removing authorization and clawback (#276)

--- token/src/event.rs
+++ token/src/event.rs
@@ -15,16 +15,6 @@ pub(crate) fn mint(e: &Env, admin: Address, to: Address, amount: i128) {
     e.events().publish(topics, amount);
 }
 
-pub(crate) fn clawback(e: &Env, admin: Address, from: Address, amount: i128) {
-    let topics = (symbol_short!("clawback"), admin, from);
-    e.events().publish(topics, amount);
-}
-
-pub(crate) fn set_authorized(e: &Env, admin: Address, id: Address, authorize: bool) {
-    let topics = (Symbol::new(e, "set_authorized"), admin, id);
-    e.events().publish(topics, authorize);
-}
-
 pub(crate) fn set_admin(e: &Env, admin: Address, new_admin: Address) {
     let topics = (symbol_short!("set_admin"), admin);
     e.events().publish(topics, new_admin);
