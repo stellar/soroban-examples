975d6c0e60fe8e3639e91c9695d3b2aede52bab8
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-01-19 22:14:19 +1000

    Move budget reset to right location

--- secp256r1/src/test.rs
+++ secp256r1/src/test.rs
@@ -20,9 +20,6 @@ fn test_wasm() {
 }
 
 fn test(env: &Env, contract_id: &Address) {
-    // Set an unlimited budget because it'll fail with the default budget.
-    env.budget().reset_unlimited();
-
     let client = ContractClient::new(env, contract_id);
 
     let key = bytesn!(&env, 0x04f7281e35b4266fd87dfc808df32354d2dfd09a645c281a94ab70f3990cb09972ecdf72c1a8d92c24abe764ac895c8a7118cf580adf33ba7d7f44f2b4cf38bba7);
@@ -31,6 +28,9 @@ fn test(env: &Env, contract_id: &Address) {
         0x0000000000000000000000000000000000000000000000000000000000000000
     );
     let sig = bytesn!(&env, 0x723da9c4c292269fb8e87054acef4c6ca8d5ba31118b7e433ce78ee079ee80043b0ee5ca5963e542f53ff257d1bd7b9c886deaec798f01b73ae86c1763ef2c48);
+
+    // Set an unlimited budget because it'll fail with the default budget.
+    env.budget().reset_unlimited();
 
     let result = client.try_verify(&key, &msg, &sig);
     assert_eq!(result, Ok(Ok(())));
