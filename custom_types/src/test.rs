33a6a5c60f189eebbd560e0924fc1d7197245585
Blaine Heffron <baheffron@gmail.com>
2024-06-26 09:32:19 -0400

    na

--- custom_types/src/test.rs
+++ custom_types/src/test.rs
@@ -8,6 +8,5 @@ fn test() {
     let env = Env::default();
     let contract_id = env.register_contract(None, CustomTypesContract);
     let client = CustomTypesContractClient::new(&env, &contract_id);
-
     assert_eq!(client.u32_fail_on_even(&1), 1);
 }
