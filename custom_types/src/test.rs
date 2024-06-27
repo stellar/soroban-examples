0288d25c58bf9ef75a37cadda84c8c63f8f385bf
Blaine Heffron <baheffron@gmail.com>
2024-06-27 13:00:57 -0400

    change custom_types contract to be equivalent to the one in soroban-cli (#313)

--- custom_types/src/test.rs
+++ custom_types/src/test.rs
@@ -6,16 +6,7 @@ use soroban_sdk::Env;
 #[test]
 fn test() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, IncrementContract);
-    let client = IncrementContractClient::new(&env, &contract_id);
-
-    assert_eq!(client.increment(&1), 1);
-    assert_eq!(client.increment(&10), 11);
-    assert_eq!(
-        client.get_state(),
-        State {
-            count: 11,
-            last_incr: 10
-        }
-    );
+    let contract_id = env.register_contract(None, CustomTypesContract);
+    let client = CustomTypesContractClient::new(&env, &contract_id);
+    assert_eq!(client.u32_fail_on_even(&1), 1);
 }
