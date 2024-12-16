2e933c2cd1c6999447d495dc6903794c17856bd7
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-12-16 22:54:13 +1000

    reorg

--- increment_with_pause/src/test_real.rs
+++ increment_with_pause/src/test_real.rs
@@ -9,7 +9,7 @@ mod pause {
 }
 
 #[test]
-fn test_notpaused() {
+fn test() {
     let env = Env::default();
 
     let pause_id = env.register(pause::WASM, ());
@@ -23,23 +23,10 @@ fn test_notpaused() {
 
     pause_client.set(&false);
     assert_eq!(client.increment(), 1);
-    assert_eq!(client.increment(), 2);
-    assert_eq!(client.increment(), 3);
-}
 
-#[test]
-fn test_paused() {
-    let env = Env::default();
-
-    let pause_id = env.register(pause::WASM, ());
-    let pause_client = pause::Client::new(&env, &pause_id);
     pause_client.set(&true);
-
-    let contract_id = env.register(
-        IncrementContract,
-        IncrementContractArgs::__constructor(&pause_id),
-    );
-    let client = IncrementContractClient::new(&env, &contract_id);
-
     assert_eq!(client.try_increment(), Err(Ok(Error::Paused)));
+
+    pause_client.set(&false);
+    assert_eq!(client.increment(), 2);
 }
