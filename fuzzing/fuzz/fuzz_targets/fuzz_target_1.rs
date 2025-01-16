6b8e68a0efae03879f70f79979c06d9d3360ca7b
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-01-16 14:40:02 +1000

    fuzzing: fix fuzz build
    ### What
    Updated the version of `soroban-sdk` to 22.0.5 and update functions being called to match those in that version.
    
    ### Why
    Fuzz code was written for a later version of the SDK than was in use.

--- fuzzing/fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzzing/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -39,7 +39,7 @@ fuzz_target!(|input: Input| {
     });
 
     // Turn off the CPU/memory budget for testing.
-    env.cost_estimate().detailed_metering().reset_unlimited();
+    env.cost_estimate().budget().reset_unlimited();
 
     let depositor_address = Address::generate(&env);
     let claimant_address = Address::generate(&env);
