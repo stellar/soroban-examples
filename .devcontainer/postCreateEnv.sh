4f8ef2f23ecb297a64d342e8866b5d4b6f24bedf
chris.anatalio <chris.anatalio@stellar.org>
2025-04-01 09:41:18 -0700

    Fix scripts

--- .devcontainer/postCreateEnv.sh
+++ .devcontainer/postCreateEnv.sh
@@ -2,16 +2,16 @@
 set -e
 
 # Make .bashrc and .zshrc writable
-chmod +w "${remoteEnv:VSCODE_HOME}/.bashrc" &&
-  echo -e "\n# Rust sccache settings" >>"${remoteEnv:VSCODE_HOME}/.bashrc" &&
-  chmod +w "${remoteEnv:VSCODE_HOME}/.zshrc" &&
-  echo -e "\n# Rust sccache settings" >>"${remoteEnv:VSCODE_HOME}/.zshrc"
+chmod +w "${remoteEnv:VSCODE_HOME}.bashrc" &&
+  echo -e "\n# Rust sccache settings" >>"${remoteEnv:VSCODE_HOME}.bashrc" &&
+  chmod +w "${remoteEnv:VSCODE_HOME}.zshrc" &&
+  echo -e "\n# Rust sccache settings" >>"${remoteEnv:VSCODE_HOME}.zshrc"
 
 # Loop and export each env var for current session
 # Append env vars to .zshrc and  for persistent usage
 echo "🎯 Exporting environment variables:"
 for env_var in "${ENV_VARS[@]}"; do
   eval "$env_var" && echo "🔹 $env_var" &&
-    echo "$env_var" >>"${remoteEnv:VSCODE_HOME}/.bashrc" &&
-    echo "$env_var" >>"${remoteEnv:VSCODE_HOME}/.zshrc"
+    echo "$env_var" >>"${remoteEnv:VSCODE_HOME}.bashrc" &&
+    echo "$env_var" >>"${remoteEnv:VSCODE_HOME}.zshrc"
 done
