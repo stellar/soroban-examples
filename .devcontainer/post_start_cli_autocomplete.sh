0c52028db967b532b9ebddfc8e0e2676ab7c3815
chris.anatalio <chris.anatalio@stellar.org>
2025-04-03 17:42:54 -0700

    Getting JSON Schema validation and script validation and build cache working on GH Actions
    Signed-off-by: chris.anatalio <chris.anatalio@stellar.org>

--- .devcontainer/post_start_cli_autocomplete.sh
+++ .devcontainer/post_start_cli_autocomplete.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-
 # Add Brew and Stellar CLI to $PATH and terminals
-chmod 644 ~/.bashrc && chmod 644 ~/.zshrc 
+chmod 644 ~/.bashrc && chmod 644 ~/.zshrc
 chmod 644 ~/.profile && chmod 644 ~/.zprofile
 test -d /home/linuxbrew/.linuxbrew  && echo "/home/linuxbrew/.linuxbrew/bin/brew" >> ~/.zprofile
-test -d /home/linuxbrew/.linuxbrew && echo "/home/linuxbrew/.linuxbrew/bin/brew" >>~/.zshrc
+test -d /home/linuxbrew/.linuxbrew && echo "/home/linuxbrew/.linuxbrew/bin/brew" >> ~/.zshrc
 
 test -d /home/linuxbrew/.linuxbrew  && echo "/home/linuxbrew/.linuxbrew/bin/brew" >> ~/.profile
-test -d /home/linuxbrew/.linuxbrew && echo "/home/linuxbrew/.linuxbrew/bin/brew" >>~/.bashrc
+test -d /home/linuxbrew/.linuxbrew && echo "/home/linuxbrew/.linuxbrew/bin/brew" >> ~/.bashrc
 
 export PATH="/home/linuxbrew/.linuxbrew/bin/brew":$PATH
 echo "export PATH='/home/linuxbrew/.linuxbrew/bin:$PATH'" >> ~/.zshrc
