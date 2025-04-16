7dcb79e7efee0a4160423845b7a7508ba917aa02
Chris.Anatalio <chris.anatalio@stellar.org>
2025-04-15 17:05:06 -0700

    Add prebuild caching images
    Signed-off-by: Chris.Anatalio <chris.anatalio@stellar.org>

--- .devcontainer/tests/build_devcontainer_ci.sh
+++ .devcontainer/tests/build_devcontainer_ci.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+
+pre_build_image=$1
+oci_pre_build_image=$2
+
 # Define devcontainer configuration directory
 devcontainer_dir=".devcontainer"
 
@@ -7,9 +11,30 @@ devcontainer_dir=".devcontainer"
 config_file="devcontainer.json"
 
 # Build the devcontainer
-devcontainer build \
+output=$(devcontainer build \
   --workspace-folder . \
-  --config $devcontainer_dir/$config_file
+  --config $devcontainer_dir/$config_file \
+  --cache-from "$pre_build_image":latest \
+  --output type=image,name="${pre_build_image}:latest")
 
+ if [ "$output" ]; then
+    image_name=$(echo "$output" | jq -r '.imageName[0]')
+    docker tag "${image_name}":latest "${pre_build_image}":latest
+    docker push "${pre_build_image}":latest
+ else
+   echo " ❌ Error building devcontainer. Please check logs above."
+   exit 1
+ fi
 
+oci_output=$(devcontainer build \
+  --workspace-folder . \
+  --config $devcontainer_dir/$config_file \
+  --cache-from "$pre_build_image":latest \
+  --cache-from "${oci_pre_build_image}:latest" \
+  --cache-to type=registry,ref="${oci_pre_build_image}:latest",mode=max,oci-artifact=true \
+  --output type=image,name="${oci_pre_build_image}:latest",mode=max,oci-mediatypes=true,compression=zstd)
 
+oci_image_name=$(echo "$oci_output" | jq -r '.imageName[0]')
+
+docker tag "${oci_image_name}":latest "${oci_pre_build_image}":latest
+docker push "${oci_pre_build_image}":latest
