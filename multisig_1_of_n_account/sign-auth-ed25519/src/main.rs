9f2f20891443f479fc61225124be8a7a60486acd
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-04-30 23:02:38 +1000

    refactored to reduce indenting

--- multisig_1_of_n_account/sign-auth-ed25519/src/main.rs
+++ multisig_1_of_n_account/sign-auth-ed25519/src/main.rs
@@ -12,6 +12,7 @@ use std::{
 };
 
 use clap::Parser;
+use colored::Colorize as _;
 use ed25519_dalek::{Keypair, Signer};
 use sha2::{Digest, Sha256};
 use stellar_xdr::curr::{
@@ -31,6 +32,7 @@ pub struct Cli {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    colored::control::set_override(true);
     let cli = Cli::parse();
 
     let network_id = Sha256::digest(cli.network_passphrase);
@@ -42,66 +44,77 @@ fn main() -> Result<(), Box<dyn Error>> {
         secret: secret_key,
         public: public_key,
     };
-    eprintln!("Public Key: {}", hex::encode(public_key.as_bytes()));
+    eprintln!(
+        "{} {}",
+        "Public Key:".cyan().bold(),
+        hex::encode(public_key.as_bytes())
+    );
 
-    let limits = Limits::none();
-    let mut limited_read = Limited::new(SkipWhitespace::new(io::stdin()), limits);
-    let mut txe = TransactionEnvelope::read_xdr_base64_to_end(&mut limited_read)?;
+    let mut txe = TransactionEnvelope::read_xdr_base64_to_end(&mut Limited::new(
+        SkipWhitespace::new(io::stdin()),
+        Limits::none(),
+    ))?;
 
-    match &mut txe {
-        TransactionEnvelope::TxV0(_) => unimplemented!(),
-        TransactionEnvelope::TxFeeBump(_) => unimplemented!(),
+    let auths = match &mut txe {
         TransactionEnvelope::Tx(e) => {
-            for op in e.tx.operations.iter_mut() {
-                match &mut op.body {
-                    OperationBody::InvokeHostFunction(op) => {
-                        for auth in op.auth.iter_mut() {
-                            eprintln!("Authorizing:\n{}", serde_json::to_string_pretty(&auth)?);
-                            match &mut auth.credentials {
-                                SorobanCredentials::Address(creds) => {
-                                    let payload = HashIdPreimage::SorobanAuthorization(
-                                        HashIdPreimageSorobanAuthorization {
-                                            network_id: Hash(network_id.try_into()?),
-                                            nonce: creds.nonce,
-                                            signature_expiration_ledger: cli
-                                                .signature_expiration_ledger,
-                                            invocation: auth.root_invocation.clone(),
-                                        },
-                                    );
-                                    let payload_xdr = payload.to_xdr(Limits::none())?;
-                                    let payload_hash = Sha256::digest(payload_xdr);
-                                    eprintln!("Payload Hash:\n{}", hex::encode(payload_hash));
-
-                                    let signature = keypair.sign(&payload_hash);
-
-                                    creds.signature_expiration_ledger =
-                                        cli.signature_expiration_ledger;
-                                    creds.signature = ScVal::Map(Some(ScMap::sorted_from([
-                                        (
-                                            ScVal::Symbol(ScSymbol("public_key".try_into()?)),
-                                            ScVal::Bytes(ScBytes(
-                                                public_key.as_bytes().try_into()?,
-                                            )),
-                                        ),
-                                        (
-                                            ScVal::Symbol(ScSymbol("signature".try_into()?)),
-                                            ScVal::Bytes(ScBytes(signature.to_bytes().try_into()?)),
-                                        ),
-                                    ])?));
-
-                                    eprintln!(
-                                        "Authorized:\n{}",
-                                        serde_json::to_string_pretty(&auth)?
-                                    );
-                                }
-                                _ => continue,
+            e.tx.operations
+                .iter_mut()
+                .filter_map(|op| match &mut op.body {
+                    OperationBody::InvokeHostFunction(op) => Some(op.auth.iter_mut().filter_map(
+                        |auth| match &mut auth.credentials {
+                            SorobanCredentials::Address(creds) => {
+                                Some((&mut auth.root_invocation, creds))
                             }
-                        }
-                    }
-                    _ => {}
-                }
-            }
+                            _ => None,
+                        },
+                    )),
+                    _ => None,
+                })
+                .flatten()
         }
+        _ => unimplemented!(),
+    };
+
+    for (invocation, creds) in auths {
+        eprintln!(
+            "{}\n{}\n{}",
+            "Authorizing:".cyan().bold(),
+            serde_json::to_string_pretty(invocation)?,
+            serde_json::to_string_pretty(creds)?,
+        );
+        let payload = HashIdPreimage::SorobanAuthorization(HashIdPreimageSorobanAuthorization {
+            network_id: Hash(network_id.try_into()?),
+            nonce: creds.nonce,
+            signature_expiration_ledger: cli.signature_expiration_ledger,
+            invocation: invocation.clone(),
+        });
+        let payload_xdr = payload.to_xdr(Limits::none())?;
+        let payload_hash = Sha256::digest(payload_xdr);
+        eprintln!(
+            "{}\n{}",
+            "Payload Hash:".cyan().bold(),
+            hex::encode(payload_hash),
+        );
+
+        let signature = keypair.sign(&payload_hash);
+
+        creds.signature_expiration_ledger = cli.signature_expiration_ledger;
+        creds.signature = ScVal::Map(Some(ScMap::sorted_from([
+            (
+                ScVal::Symbol(ScSymbol("public_key".try_into()?)),
+                ScVal::Bytes(ScBytes(public_key.as_bytes().try_into()?)),
+            ),
+            (
+                ScVal::Symbol(ScSymbol("signature".try_into()?)),
+                ScVal::Bytes(ScBytes(signature.to_bytes().try_into()?)),
+            ),
+        ])?));
+
+        eprintln!(
+            "{}\n{}",
+            "Authorized:".green().bold(),
+            serde_json::to_string_pretty(creds)?,
+        );
     }
 
     println!("{}", txe.to_xdr_base64(Limits::none())?);
