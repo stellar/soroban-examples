778e1317f62d35553cd9211818913ad4b8667cd4
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-04-30 21:27:55 +1000

    it works

--- multisig_1_of_n_account/README.md
+++ multisig_1_of_n_account/README.md
@@ -1,20 +1,37 @@
 
 ## How to use this contract
 
+The example below sets up an asset with an admin, where that admin can sign for any authorization with ed25519 keys.
+
+The ed25519 keys used in the example below are:
+
+- Secret Key: `0000000000000000000000000000000000000000000000000000000000000000`  
+  Public Key: `3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29`
+- Secret Key: `0000000000000000000000000000000000000000000000000000000000000001`  
+  Public Key: `4cb5abf6ad79fbf5abbccafcc269d85cd2651ed4b885b5869f241aedf0a5ba29`
+
+These keys are publicly viewable and not random. Do not use these keys for any purpose. Select your own keys.
+
+### Install the sign-ed25519 cli plugin
+
+```
+cd sign-ed25519
+cargo install --locked --path .
+```
+
 ### Deploy admin
 
 ```
 cd contract
 stellar contract build --out-dir out/
 stellar contract deploy \
-  --alias admin \
-  --wasm out/soroban_multisig_1_of_n_account_contract.wasm
-  -- \
-  --signers '[
-    "",
-    ""
-  ]'
-cd ..
+    --alias admin \
+    --wasm out/soroban_multisig_1_of_n_account_contract.wasm
+    -- \
+    --signers '[
+      "3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29",
+      "4cb5abf6ad79fbf5abbccafcc269d85cd2651ed4b885b5869f241aedf0a5ba29"
+    ]'
 ```
 
 ### Deploy asset
@@ -22,8 +39,8 @@ cd ..
 ```
 stellar keys generate issuer
 stellar contract asset deploy \
-  --alias asset \
-  --asset ABC:issuer
+    --alias asset \
+    --asset ABC:issuer
 ```
 
 ### List of contracts
@@ -36,18 +53,31 @@ stellar contract alias ls
 
 ```
 stellar contract invoke --id asset -- \
-  set_admin \
-  --new_admin admin
+    set_admin \
+    --new_admin admin
 ```
 
 ### Mint
 
+Call mint, sending to an address. The invocation will be signed by the admin using one of the ed25519 keys set in the constructor. Choose which key signs by setting the `SECRET_KEY` environment variable.
+
 ```
 stellar contract invoke --id asset -- \
-  mint \
-  --to CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABSC4 \
-  --amount 123
+    mint \
+    --to CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABSC4 \
+    --amount 123 \
+  | stellar tx simulate \
+  | tr -d '\n' \
+  | SECRET_KEY=0000000000000000000000000000000000000000000000000000000000000001 \
+      stellar sign-ed25519 \
+  | stellar tx simulate \
+  | stellar tx send
 ```
 
+### View Balance
 
-
+```
+stellar contract invoke --id asset -- \
+    balance \
+    --id CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABSC4
+```
---
+++ multisig_1_of_n_account/sign-ed25519/src/main.rs
@@ -0,0 +1,86 @@
+//! This tool receives via stdin a base64 encoded transaction envelope and returns the envelope
+//! to stdout with any contract authorizations modified to contain a signature with the key
+//! hardcoded in the tool transaction.
+
+use std::{env, error::Error, io};
+
+use ed25519_dalek::{Keypair, Signer};
+use sha2::{Digest, Sha256};
+use stellar_xdr::curr::{
+    Hash, HashIdPreimage, HashIdPreimageSorobanAuthorization, Limited, Limits, OperationBody,
+    ReadXdr, ScBytes, ScMap, ScSymbol, ScVal, SorobanCredentials, TransactionEnvelope, WriteXdr,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let secret_key_hex_str = env::var("SECRET_KEY")?;
+    let network_passphrase = env::var("NETWORK_PASSPHRASE")?;
+    let network_id = Sha256::digest(network_passphrase);
+
+    let secret_key_bytes = hex::decode(secret_key_hex_str)?;
+    let secret_key = ed25519_dalek::SecretKey::from_bytes(&secret_key_bytes)?;
+    let public_key = ed25519_dalek::PublicKey::from(&secret_key);
+    let keypair = Keypair {
+        secret: secret_key,
+        public: public_key,
+    };
+    eprintln!("Public Key: {}", hex::encode(public_key.as_bytes()));
+
+    let limits = Limits::none();
+    let mut limited_read = Limited::new(io::stdin(), limits);
+    let mut txe = TransactionEnvelope::read_xdr_base64_to_end(&mut limited_read)?;
+
+    match &mut txe {
+        TransactionEnvelope::TxV0(_) => unimplemented!(),
+        TransactionEnvelope::TxFeeBump(_) => unimplemented!(),
+        TransactionEnvelope::Tx(e) => {
+            for op in e.tx.operations.iter_mut() {
+                match &mut op.body {
+                    OperationBody::InvokeHostFunction(op) => {
+                        for auth in op.auth.iter_mut() {
+                            eprintln!("Authorizing:\n{}", serde_json::to_string_pretty(&auth)?);
+                            match &mut auth.credentials {
+                                SorobanCredentials::Address(creds) => {
+                                    let payload = HashIdPreimage::SorobanAuthorization(
+                                        HashIdPreimageSorobanAuthorization {
+                                            network_id: Hash(network_id.try_into()?),
+                                            nonce: creds.nonce,
+                                            signature_expiration_ledger: creds
+                                                .signature_expiration_ledger,
+                                            invocation: auth.root_invocation.clone(),
+                                        },
+                                    );
+                                    let payload_xdr = payload.to_xdr(Limits::none())?;
+                                    let payload_hash = Sha256::digest(payload_xdr);
+                                    eprintln!("Payload Hash:\n{}", hex::encode(payload_hash));
+                                    let signature = keypair.sign(&payload_hash);
+                                    creds.signature = ScVal::Map(Some(ScMap::sorted_from([
+                                        (
+                                            ScVal::Symbol(ScSymbol("public_key".try_into()?)),
+                                            ScVal::Bytes(ScBytes(
+                                                public_key.as_bytes().try_into()?,
+                                            )),
+                                        ),
+                                        (
+                                            ScVal::Symbol(ScSymbol("signature".try_into()?)),
+                                            ScVal::Bytes(ScBytes(signature.to_bytes().try_into()?)),
+                                        ),
+                                    ])?));
+                                    eprintln!(
+                                        "Authorized:\n{}",
+                                        serde_json::to_string_pretty(&auth)?
+                                    );
+                                }
+                                _ => continue,
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    println!("{}", txe.to_xdr_base64(Limits::none())?);
+
+    Ok(())
+}
